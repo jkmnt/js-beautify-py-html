d263e19be3f49a4c27cf5f068a40dc404bacf0af
jkmnt <git@firewood.fastmail.com>
2024-07-05 08:35:59 +0300

    smoke changed

--- python/test-htmlbeautifier-smoke.py
+++ python/test-htmlbeautifier-smoke.py
@@ -1,14 +1,11 @@
 from jsbeautifier.html.beautifier import Beautifier
 
-text = """<div><div></div><div><div></div></div></div>
-
-"""
-
-text = """
+SOUP = """
 
         <div>
         <!-- soup -->
                 <span>Foo</span><span>Bar</span>
+                {% it's the template <div><div></div></div> %}
                 <div>
                     <div attr="1" attr="1"
 
@@ -26,8 +23,9 @@ INLINE   </span>      </div     >
             </div>
 """
 
+
 b = Beautifier(
-    text,
+    SOUP,
     {
         "indent_size": "4",
         "indent_char": " ",
@@ -42,6 +40,8 @@ b = Beautifier(
         "jslint_happy": False,
         "end_with_newline": False,
         "wrap_line_length": "80",
+        "wrap-attributes": "preserve-aligned",
+        "wrap_attributes_indent_size": 4,
         "indent_inner_html": False,
         "comma_first": False,
         "e4x": False,
