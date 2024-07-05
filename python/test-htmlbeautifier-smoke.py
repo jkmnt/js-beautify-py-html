424b58faadccf09cd367c957a101d71a93ca2bab
jkmnt <git@firewood.fastmail.com>
2024-07-05 05:41:20 +0300

    sfsg

--- python/test-htmlbeautifier-smoke.py
+++ python/test-htmlbeautifier-smoke.py
@@ -2,16 +2,28 @@ from jsbeautifier.html.beautifier import Beautifier
 
 
 b = Beautifier("""
-    <!-- soup -->
+
         <div>
+        <!-- soup -->
 <span>Foo</span><span>Bar</span>
 <div>
-    <div attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" >
+    <div attr="1" attr="1"
+
+        attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" attr="1" >
     Bla
             </div>
-</div>
+
+
+
+
+</div><div>a</div><div>a</div><div>a</div><div>a</   div><      div>
+
+a            <span>
+
+
+INLINE   </span>      </      div     >
             </div>
-""")
+""", {"wrap_attributes": "force-aligned", "max_preserve_newlines": 1, "wrap_line_length": 80})
 res = b.beautify()
 
 print(res)
